cc9376f6057b0693375567c642540d740fcb3601
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-03-07 16:26:51 +0100

    Product Card Update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -14,12 +14,36 @@ const Product = ({product}) => {
         <div className="border border-[#e4e4e4] h-[300px] 
         mb-4 relative overflow-hidden group transition">
           <div className="w-full h-full flex justify-center items-center">
-            <div>
-              <img src={image} alt="" />
+            <div className="w-[200px] mx-auto flex justify-center items-center">
+              <img className="max-h-[160px] group-hover:scale-110 
+              transition duration-300 " 
+              src={image} 
+              alt="" 
+              />
             </div>
           </div>   
+          <div className="absolute top-6 -right-11 group-hover:right-5 bg-red-500/40 p-2 flex flex-col 
+          items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 
+          transition-all duration-300">
+            <button>
+              <div className="flex justify-center items-center text-white
+              w-12 h-12 bg-red-500/40">
+                <BsPlus className="text-3xl"/>
+              </div>
+            </button>
+            <Link to={`/product/${id}`} className="w-12 h-12 bg-white flex justify-center
+            items-center text-primary drop-shadow-xl">
+              <BsEyeFill />
+            </Link>
+          </div>
         </div>
-        <div>1</div>
+        <div>
+          <div className="text-sm capitalize text-gray-500 mb-1 ">{category}</div>
+          <Link to={`/product/${id}`}>
+            <h2 className="font-semibold mb-1 ">{title}</h2>
+          </Link>
+          <div className="font-semibold ">{price} €</div>
+        </div>
       </div>
     )
   }
