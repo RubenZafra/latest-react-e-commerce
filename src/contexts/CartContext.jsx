53b465bddd4554907dc132f931b88c6fa9b5a2cb
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-03-08 09:56:48 +0100

    Added Remove From Cart Func

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -28,10 +28,13 @@ const CartProvider = ({children}) => {
     }
   }
 
-  console.log(cart)
+  const removeFromCart = (id) => {
+    const newCart = cart.filter(item => item.id !== id)
+    setCart(newCart)
+  }
 
   return (
-    <CartContext.Provider value={{cart, addToCart}}>
+    <CartContext.Provider value={{cart, addToCart, removeFromCart}}>
       {children}
     </CartContext.Provider>
     )
